63b2b4b91985fea0d47e85792316d3ccd928b896
jabernall24 <jabernall24@gmail.com>
2019-11-03 18:11:36 -0800

    added routes

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const validator = require("email-validator");
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Connecting to db (below) ====================================================
 
 const postgresql = require('pg');
@@ -11,15 +17,46 @@ client.connect();
 
 // Connecting to db(above) ====================================================
 
-app.engine("html", require('ejs').renderFile);
-app.use(express.static("public"));
+// USER
+// ========================================================================================================
+// Create user
+app.post('/users/add', function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const category = req.body.category;
 
-app.get("/categories/all", function(req, res) {
+    if(!validator.validate(email)) {
+        return res.status(400).json({"message": "Invalid Email"});
+    }
+
+    const CATEGORIES = ['Computer Science']
+
+    if (CATEGORIES.indexOf(category) <= -1) {
+        return res.status(400).json({"message": "Invalid Category"});
+    } 
+
     client
-        .query('SELECT * FROM category WHERE cat = $1::text', ['Computer Science'])
+        .query('INSERT INTO users(email, pass, q_cat) values($1::text, $2::text, $3::text);', [email, password, category])
+        .then(result => res.status(200).json(result.rows))
+        .catch(e => {
+            if(e.code == "23505") {
+                return res.status(400).json({"message": "Email already exists."});
+            } else {
+                return res.status(400).json({"message": e});
+            }
+        })
+});
+
+// Get user with email and password
+app.post('/users/login', async function(req, res) {
+    console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+
+    await client
+        .query("SELECT * FROM users WHERE email = $1::text AND pass = $2::text", [email, password])
         .then(result => res.status(200).json(result.rows))
         .catch(e => res.status(400).json(e))
-        .then(() => client.end())
 });
 
 // process.env.PORT
--- schemas.sql
+++ schemas.sql
@@ -1,45 +1,73 @@
 CREATE DATABASE test;
 
+DROP TABLE questions;
+DROP TABLE groups;
+DROP TABLE users;
+DROP TABLE category;
+
 CREATE TABLE category(
-    cat TEXT NOT NULL UNIQUE,
-    sub_cat TEXT NOT NULL UNIQUE,
+    cat TEXT NOT NULL,
+    sub_cat TEXT NOT NULL,
+    UNIQUE(cat, sub_cat),
     PRIMARY KEY (cat, sub_cat)
 );
 
 CREATE TABLE users(
-    id SERIAL UNIQUE,
-    email TEXT NOT NULL,
+    user_id SERIAL,
+    email TEXT NOT NULL UNIQUE,
     pass TEXT NOT NULL,
-    q_cat TEXT REFERENCES category(cat) ON DELETE CASCADE,
-    PRIMARY KEY(id)
+    is_subscribed BOOLEAN NOT NULL,
+    cat TEXT NOT NULL,
+    sub_cat TEXT[5] NOT NULL, -- Unofficial reference to the category table(every sub_cat must be from the same category)
+    PRIMARY KEY(user_id)
 );
 
 CREATE TABLE groups(
-    id SMALLINT,
-    company TEXT UNIQUE,
-    PRIMARY KEY (id)
+    group_id SERIAL,
+    company TEXT NOT NULL UNIQUE,
+    PRIMARY KEY (group_id)
 );
 
 CREATE TABLE questions(
-    id SMALLINT,
-    category TEXT REFERENCES category(cat) ON DELETE CASCADE,
-    sub_cat TEXT REFERENCES category(sub_cat) ON DELETE CASCADE,
-    difficulty INTEGER,
-    groups INTEGER REFERENCES groups(id) ON DELETE CASCADE,
-    rating DECIMAL,
+    question_id SERIAL,
+    title TEXT NOT NULL UNIQUE,
+    difficulty INTEGER CHECK(difficulty >= 1 AND difficulty <= 5),
+    groups INTEGER REFERENCES groups(group_id) ON DELETE CASCADE,
     rating_counter INTEGER,
-    PRIMARY KEY (id)
+    rating DECIMAL,
+    cat TEXT NOT NULL,
+    sub_cat TEXT NOT NULL,
+    FOREIGN KEY (cat, sub_cat) REFERENCES category(cat, sub_cat) ON DELETE CASCADE,
+    PRIMARY KEY (question_id)
 );
 
 INSERT INTO category(cat, sub_cat) values('Computer Science', 'Arrays');
 INSERT INTO category(cat, sub_cat) values('Computer Science', 'Sets');
 INSERT INTO category(cat, sub_cat) values('Computer Science', 'Hash Maps');
 INSERT INTO category(cat, sub_cat) values('Computer Science', 'Graphs');
-INSERT INTO category(cat, sub_cat) values('Computer Science', 'Recursion');
+INSERT INTO category(cat, sub_cat) values('Computer Science', 'Sorting');
 
-INSERT INTO users(email, pass, cat) values('jabernall24@gmail.com', 'fuckidk', 'Computer Science');
-INSERT INTO users(email, pass, cat) values('mikemenendez@gmail.com', 'lkasjef', 'Computer Science');
+INSERT INTO users(email, pass, is_subscribed, cat, sub_cat) values('test1@gmail.com', '123456', true, 'Computer Science', '{"Arrays"}');
+INSERT INTO users(email, pass, is_subscribed, cat, sub_cat) values('test2@gmail.com', '123456', true, 'Computer Science', '{"Sets", "Sorting"}');
+INSERT INTO users(email, pass, is_subscribed, cat, sub_cat) values('test3@gmail.com', '123456', true, 'Computer Science', '{"Graphs"}');
+INSERT INTO users(email, pass, is_subscribed, cat, sub_cat) values('test4@gmail.com', '123456', true, 'Computer Science', '{"Graphs"}');
+INSERT INTO users(email, pass, is_subscribed, cat, sub_cat) values('test5@gmail.com', '123456', true, 'Computer Science', '{"Graphs"}');
+INSERT INTO users(email, pass, is_subscribed, cat, sub_cat) values('test6@gmail.com', '123456', false, 'Computer Science', '{"Hash Maps"}');
 
+INSERT INTO groups(company) values('Regular');
+
+INSERT INTO questions(title, cat, sub_cat, difficulty, groups, rating, rating_counter) 
+            values('Implement quicksort', 'Computer Science', 'Sorting', 1, 1, 3, 1);
+INSERT INTO questions(title, cat, sub_cat, difficulty, groups, rating, rating_counter) 
+            values('Implement merge sort', 'Computer Science', 'Sorting', 1, 1, 3.5, 2);
+INSERT INTO questions(title, cat, sub_cat, difficulty, groups, rating, rating_counter) 
+            values('Insert into a BST', 'Computer Science', 'Graphs', 1, 1, 3, 2);
+INSERT INTO questions(title, cat, sub_cat, difficulty, groups, rating, rating_counter) 
+            values('Implement isBST', 'Computer Science', 'Graphs', 1, 1, 4, 3);
+INSERT INTO questions(title, cat, sub_cat, difficulty, groups, rating, rating_counter) 
+            values('Check anagram', 'Computer Science', 'Hash Maps', 1, 1, 3, 2);
+INSERT INTO questions(title, cat, sub_cat, difficulty, groups, rating, rating_counter) 
+            values('Check palindrome', 'Computer Science', 'Arrays', 1, 1, 3.3, 10);
 -- :::NO SQL::: --
 
 -- question:
