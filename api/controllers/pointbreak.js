e171690d6798f476cd3bc85c514d138f5d248557
jabernall24 <jabernall24@gmail.com>
2019-12-12 03:33:20 -0800

    test

--- api/controllers/pointbreak.js
+++ api/controllers/pointbreak.js
@@ -1,7 +1,7 @@
 // const client = require("../../db/db").client;
 // const dynamoDB = require("../../db/db").dynamoDB;
-// var fs = require("fs");
-// var docker = require("dockerode");
+var fs = require("fs");
+var docker = require("dockerode");
 // var exec = require("child_process").exec;
 // var codeMap = {
 //     ".java" : "JAVA DOCKER FILE PATH",
@@ -14,8 +14,31 @@ exports.pointbreak = async (req, res) => {
 	// console.log("HERE");
 	
 	let body = req.body.code;
-	console.log(body);
-	return res.status(200).json(body);
+	
+	body = body.join("\n");
+
+	// const fs = require("fs");
+
+	let file_path = "/tmp/test";
+
+	fs.writeFile(file_path, body, async function(err) {
+
+		if(err) {
+			return res.status(400).json(err);
+		}
+
+		await mikesfunction(file_path,"main.cpp");
+
+		fs.readFile("./out.txt", (err, data) =>{
+			if(err) return res.status(400).json(err);
+			if(data.toString() === "Hello World!\n"){
+				return res.status(200).json({"message":true});
+			}
+			else{
+				return  res.status(400).json({"message": false});
+			}
+		});
+	}); 
 	// let file_path =  req.files.main.tempFilePath;
 	// console.log(file_path);
 	// let fname = req.files.main.name;
@@ -36,42 +59,42 @@ exports.pointbreak = async (req, res) => {
     
 };
 
-// async function mikesfunction(file_path, fname) {
-// 	var start = "docker run -it -d --name=test user:cpp";
-// 	var f_to_c = "docker cp " + file_path + "/" + fname + " test:.";
-// 	var compile = "docker exec test g++ " + fname;
-// 	var execute = "docker exec test sh -c './a.out > out.txt'";
-// 	var c_to_f = "docker cp test:/out.txt .";
-// 	var stop = "docker stop test";
-// 	var rm = "docker rm test";
+async function mikesfunction(file_path, fname) {
+	var start = "docker run -it -d --name=test user:cpp";
+	var f_to_c = "docker cp " + file_path + "/" + fname + " test:.";
+	var compile = "docker exec test g++ " + fname;
+	var execute = "docker exec test sh -c './a.out > out.txt'";
+	var c_to_f = "docker cp test:/out.txt .";
+	var stop = "docker stop test";
+	var rm = "docker rm test";
 
-// 	await run(start);
-// 	await run(f_to_c);
-// 	await run(compile);
-// 	await run(execute);
-// 	await run(c_to_f);
-// 	await run(stop);
-// 	await run(rm);
+	await run(start);
+	await run(f_to_c);
+	await run(compile);
+	await run(execute);
+	await run(c_to_f);
+	await run(stop);
+	await run(rm);
 
-// }
+}
 
-// function run(cmd){
-// 	return new Promise((resolve, reject) => {
-// 		try{
-// 			exec(cmd, function(err, out, stderr) {
-// 				if (err) {
-// 					console.log(err);
-// 					return reject(err);
+function run(cmd){
+	return new Promise((resolve, reject) => {
+		try{
+			exec(cmd, function(err, out, stderr) {
+				if (err) {
+					console.log(err);
+					return reject(err);
 
-// 				}
-// 				console.log(out);
-// 				console.log(stderr);
-// 				return resolve(out);
-// 			}); 
-// 		}  
-// 		catch(err){
-// 			console.log(err);
-// 			throw err;
-// 		}
-// 	});
-// }
+				}
+				console.log(out);
+				console.log(stderr);
+				return resolve(out);
+			}); 
+		}  
+		catch(err){
+			console.log(err);
+			throw err;
+		}
+	});
+}
