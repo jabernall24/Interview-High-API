2da5b1d0a57844518dbb54d0b5280809b5c2748c
jabernall24 <jabernall24@gmail.com>
2020-01-20 19:22:06 -0800

    easier to rename container

--- api/controllers/pointbreak.js
+++ api/controllers/pointbreak.js
@@ -15,6 +15,7 @@ exports.pointbreak = async (req, res) => {
 	
 	let body = req.body.code;
 	const user_id = req.body.user_id;
+	const question_id = req.body.question_id;
 	
 	body = body.join("\n");
 
@@ -23,7 +24,7 @@ exports.pointbreak = async (req, res) => {
 	fs.writeFile("main.cpp", data, async (err) => {
 		if (err) return res.status(400).json("JENR: " + err);
 		
-		await mikesfunction("./","main.cpp", user_id);
+		await mikesfunction("./","main.cpp", user_id, question_id);
 
 		fs.readFile("./out.txt", (err, data) =>{
 			if(err) return res.status(400).json("MY ERROR: " + err);
@@ -37,14 +38,15 @@ exports.pointbreak = async (req, res) => {
 	});
 };
 
-async function mikesfunction(file_path, fname, user_id) {
-	const start = "docker run -it -d --name=test_" + user_id + " user:cpp";
-	const f_to_c = "docker cp " + file_path + "/" + fname + " test_" + user_id + ":.";
-	const compile = "docker exec test_" + user_id + " g++ " + fname;
-	const execute = "docker exec test_" + user_id + " sh -c './a.out > out.txt'";
-	const c_to_f = "docker cp test_" + user_id + ":/out.txt .";
-	const stop = "docker stop test_" + user_id;
-	const rm = "docker rm test_" + user_id;
+async function mikesfunction(file_path, fname, user_id, question_id) {
+	const container_name = user_id + "_" + question_id;
+	const start = "docker run -it -d --name=" + container_name + " user:cpp";
+	const f_to_c = "docker cp " + file_path + "/" + fname + " " + container_name + ":.";
+	const compile = "docker exec " + container_name + " g++ " + fname;
+	const execute = "docker exec " + container_name + " sh -c './a.out > out.txt'";
+	const c_to_f = "docker cp " + container_name + ":/out.txt .";
+	const stop = "docker stop " + container_name;
+	const rm = "docker rm " + container_name;
 
 	await run(start, false);
 	await run(f_to_c, false);
